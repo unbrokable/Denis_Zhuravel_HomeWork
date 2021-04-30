01328a31a489e7be9aa599359e135080ba25f569
unbrokable <80967885+unbrokable@users.noreply.github.com>
2021-04-30 19:01:30 +0300

    AddPz1

---
+++ Pz1/DI.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using DI.App.Abstractions;
+using DI.App.Services;
+using DI.App.Services.PL;
+using DI.App.Services.PL.Commands;
+
+namespace DI.App
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            // Inversion of Control
+
+            var addUsers = new AddUserCommand(new UserStore(new InMemoryDatabaseService()));
+            var listUsers = new ListUsersCommand(new UserStore(new InMemoryDatabaseService()));
+            ICommandProcessor processor = new CommandProcessor(new ICommand[] { addUsers, listUsers });
+            var manager = new CommandManager(processor);
+            manager.Start();
+
+        }
+    }
+}
---
+++ Pz1/DI.App/Services/PL/Commands/CommandProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DI.App.Abstractions;
+using DI.App.Services.PL.Commands;
+
+namespace DI.App.Services.PL
+{
+    public class CommandProcessor : ICommandProcessor
+    {
+        private readonly Dictionary<int, ICommand> commands = new Dictionary<int, ICommand>();
+
+        public CommandProcessor(  ICommand[] commands)
+        {
+          for(int i = 0; i < commands.Length; i++)
+            {
+                this.commands.Add(commands[i].Number, commands[i]);
+
+            }
+        }
+
+        public void Process(int number)
+        {
+            if (!this.commands.TryGetValue(number, out var command)) return;
+
+            command.Execute();
+        }
+
+        public IEnumerable<ICommand> Commands => this.commands.Values.AsEnumerable();
+    }
+}
