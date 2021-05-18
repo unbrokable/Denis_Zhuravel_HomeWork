6597dafab2737bfde4628663f6693482db1762c0
unbrokable <80967885+unbrokable@users.noreply.github.com>
2021-05-18 22:48:53 +0300

    Add solution

---
+++ FileManager/FileManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, IReader> readers = new Dictionary<string, IReader>()
+            {
+                {"xml",new ReaderXml() },
+                {"json", new ReaderJson() },
+                {"txt", new ReaderText()}
+            };
+            FileManager fileManager = new FileManager(@"c:\", readers, new ReaderAnorher());
+            fileManager.RunManager();
+
+        }
+    }
+}
